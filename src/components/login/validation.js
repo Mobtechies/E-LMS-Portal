e2c0bbfe4816fb73c22c85d164f000cff153b04e
Usama <javaidusama60@gmail.com>
2021-05-30 21:59:07 +0500

    Version 1.0

---
+++ src/components/login/validation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Validation extends React.Component{
+    constructor()
+    {
+        super();
+        this.state={
+            name:"",
+            password: "",
+        }
+     
+    }
+}
+export default Validation;
