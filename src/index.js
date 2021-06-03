4ac1f95c25fecbe49bfe802a85386f4e39ff8dd3
Usama <javaidusama60@gmail.com>
2021-06-04 00:28:05 +0500

    commit 3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import App from './App'
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '.'
+
 
 
 ReactDOM.render(
